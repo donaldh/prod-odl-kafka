e6f16f9b86ae1a05194e85a434dfb66680b9b970
Willis Chen <williscc@cisco.com>
2016-05-09 12:07:21 +0100

    add headers

--- kafka-agent/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/kafka/agent/impl/rev150922/NoopDOMConsumer.java
+++ kafka-agent/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/kafka/agent/impl/rev150922/NoopDOMConsumer.java
@@ -1,6 +1,19 @@
-/*
- * [To Do: Use OpenDayLight licence]
- */
+/************************************************************************************************************
+* Copyright (c) 2016 Cisco and/or its affiliates.
+* This software is licensed to you under the terms of the Apache License, Version 2.0 (the "License").
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+* The code, technical concepts, and all information contained herein, are the property of Cisco Technology, Inc.
+* and/or its affiliated entities, under various laws including copyright, international treaties, patent,
+* and/or contract. Any use of the material herein must be in accordance with the terms of the License.
+* All rights not expressly granted by the License are reserved.
+* Unless required by applicable law or agreed to separately in writing, software distributed under the
+* License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+* either express or implied.
+* 
+
+* Name:       NoopDOMConsumer.java
+* Purpose:    DOM message consumer class
+*************************************************************************************************************/
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.kafka.agent.impl.rev150922;
 
 import java.util.Collection;
@@ -11,10 +24,7 @@ import org.opendaylight.controller.sal.core.api.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * 
- * @author Xiaoyu Chen
- */
+
 public class NoopDOMConsumer implements Consumer {
 
     private static final Logger LOG = LoggerFactory.getLogger(NoopDOMConsumer.class);
